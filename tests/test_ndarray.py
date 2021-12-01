Update CMakeLists and fix work group overflow
* CMakeLists
  * Re-enable CUDA compilation
  * Note: there may still be an issue with which
  version of OpenCL the project binds to. It may
  bind to the CUDA version when CUDA is built.
* opencl backend
  * Remove define variables that are not defined in
  the (likely) CUDA version of OpenCL
  * Fix workgroup size calculation (use the max size)
* Update test_ndarray simple test to handle above issue

tests/test_ndarray.py
@@ -29,14 +29,18 @@ def check_same_memory(original, view):
 
 # Custom opencl tests
 def test_simple_opencl():
-  shape, np_fn, nd_fn = (4, 4), lambda X: X.transpose(), lambda X: X.permute((1, 0))
-  _A = np.random.randint(low=0, high=10, size=shape)
-  print(_A)
-  A = nd.array(_A, device=nd.opencl())
-  print(A)
-  print(A.numpy())
-  A.fill(1.)
-  print(A)
+  A = nd.array(np.random.randn(2049), device=nd.opencl())
+  B = nd.array(np.random.randn(2049), device=nd.opencl())
+  print(A+B)
+  print(A.shape)
+  # shape, np_fn, nd_fn = (4, 4), lambda X: X.transpose(), lambda X: X.permute((1, 0))
+  # _A = np.random.randint(low=0, high=10, size=shape)
+  # print(_A)
+  # A = nd.array(_A, device=nd.opencl())
+  # print(A)
+  # print(A.numpy())
+  # A.fill(1.)
+  # print(A)
   raise ValueError()
 
 